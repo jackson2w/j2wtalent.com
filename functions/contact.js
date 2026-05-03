ccc1e4d7c32331936e497df09771d67951d01438
Willie Jackson <willie@williejackson.com>
2026-05-02 23:54:47 -0500

    rebuilt pages using grt

--- functions/contact.js
+++ functions/contact.js
@@ -160,5 +160,6 @@ function esc(str) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
